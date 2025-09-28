ae702caa87296f25d3095c7f006627ce8275c3c1
babitha2005 <126014025@sastra.ac.in>
2025-09-28 19:05:20 +0530

    Add files via upload

---
+++ Task.java
@@ -0,0 +1,18 @@
+package com.example.taskapi.model;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Document(collection = "tasks")
+public class Task {
+    @Id
+    private String id;
+    private String name;
+    private String owner;
+    private String command;
+    private List<TaskExecution> taskExecutions = new ArrayList<>();
+}
